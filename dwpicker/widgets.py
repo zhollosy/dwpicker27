174496ec7fba9b4d41885b76fded418d1fa300b9
Zola <zoltan.hollosy@digicpictures.com>
2025-01-30 19:06:33 +0100

    MERGE resolved

--- dwpicker/widgets.py
+++ dwpicker/widgets.py
@@ -1,6 +1,6 @@
 
 import sys
-from functools import partial, lru_cache
+from functools import partial
 from dwpicker.pyside import QtGui, QtCore, QtWidgets
 from dwpicker.compatibility import ensure_general_options_sanity
 from dwpicker.colorwheel import ColorDialog
@@ -13,8 +13,8 @@ from dwpicker.stack import count_panels
 # don't use style sheet like that, find better design
 TOGGLER_STYLESHEET = (
     'background: rgb(0, 0, 0, 75); text-align: left; font: bold')
-X = '✘'
-V = '✔'
+X = u'\u2717' # for x
+V = u'\u2713' # for checkmark
 
 
 class BoolCombo(QtWidgets.QComboBox):
@@ -514,7 +514,6 @@ class ZoomLockedModel(QtCore.QAbstractTableModel):
                 return str(general['panels.names'][index.row()])
 
 
-@lru_cache()
 def get_color_icon(color, size=None, as_pixmap=False):
     px = QtGui.QPixmap(QtCore.QSize(*(size if size else (64, 64))))
     px.fill(QtCore.Qt.transparent)
